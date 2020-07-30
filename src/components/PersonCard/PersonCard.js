83d2db71f3641956c2338c3c1e26fcd3360209d1
Hursh <hursharora@gmail.com>
2020-07-29 19:45:41 -0700

    dynamically create PersonCards

--- src/components/PersonCard/PersonCard.js
+++ src/components/PersonCard/PersonCard.js
@@ -2,17 +2,16 @@ import React from "react";
 import TempItem from "../TempItem/TempItem";
 import ListDragDrop from "@bit/cy-shan.fancy-data-manipulators.list-drag-drop";
 import classes from "./PersonCard.module.css";
+import { BigHead } from "@bigheads/core";
 
 const PersonCard = props => {
     return (
         <div className={classes.Card}>
             <h3 className={classes.CardName}>Person {props.number}</h3>
             <div className={classes.CardInner}>
-                <img
-                    src="https://bigheads.io/svg"
-                    alt="generated avatar"
-                    className={classes.AvatarImage}
-                />
+                <div className={classes.AvatarImage}>
+                    <BigHead />
+                </div>
                 <div className={classes.OrderList}>
                     <ListDragDrop
                         items={props.data}

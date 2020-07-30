b41bc271e5d787297b0bbed00c857f2e414d22ea
Hursh <hursharora@gmail.com>
2020-07-29 19:45:13 -0700

    add preference reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import PersonCard from "./components/PersonCard/PersonCard";
 import CardRow from "./components/CardRow/CardRow";
 
-const App = () => {
-    const data = [];
-    for (let i = 0; i < 5; i++) {
-        data.push({
-            id: i,
-            name: "Person " + (i + 1)
-        });
+const initPreferences = () => {
+    const initPreferences = [];
+    for (let i = 0; i < 10; i++) {
+        let curr = [];
+        for (let j = 0; j < 5; j++) {
+            curr.push(j);
+        }
+        initPreferences.push(curr);
     }
+    return initPreferences;
+};
+
+const preferenceReducer = (currentIngredients, action) => {
+    switch (action.type) {
+        case "SET":
+            return action.ingredients;
+        case "RANDOMIZE":
+            return [...currentIngredients, action.ingredient];
+        case "RESET":
+            return initPreferences();
+        default:
+            throw new Error("Invalid action type");
+    }
+};
+
+const App = () => {
+    const [preferenceData, dispatch] = useReducer(
+        preferenceReducer,
+        [],
+        initPreferences
+    );
 
     return (
-        <CardRow>
-            <PersonCard data={data} number={1} />
-            <PersonCard data={data} number={2} />
-            <PersonCard data={data} number={3} />
-            <PersonCard data={data} number={4} />
-            <PersonCard data={data} number={5} />
-        </CardRow>
+        <>
+            <CardRow prefData={preferenceData.slice(0, 5)} />
+            <CardRow prefData={preferenceData.slice(5, 10)} />
+        </>
     );
 };
 
